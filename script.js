d6f14fbfe91758015b52821e9613dd9060f4e57e
Deepak <kandpaldeepak100@gmail.com>
2023-02-07 08:57:34 +0530

    initial commit

--- script.js
+++ script.js
@@ -1,75 +1,115 @@
-console.log("hello Welcome t spotify Clone")
+
+console.log("hello Welcome t spotify");
 
 //Initialize The Variables--------------------------------------------------
-let music = '';
-let Index = 0;
 let masterPlay = document.getElementById('masterPlay');
 let progressBar = document.getElementById('myProgressBar');
+let masterSong = document.getElementById('masterSong');
 let gif = document.getElementById('gif');
+let progressTime = document.getElementById('currentTime')
 // let timeStamp = document.querySelectorAll('timeStamp');
 let songItems = Array.from(document.getElementsByClassName('songItem'));
 let songs = [
-    { songName: "8 Letters Why don t we", songPath: "songs/8 Letters Why Don t We.mp3", songCover: "covers/2.jpg" },
-    { songName: "Anti Hero", songPath: "songs/Anti Hero.mp3", songCover: "covers/2.jpg" },
-    { songName: "closer", songPath: "songs/closer.mp3", songCover: "covers/3.jpg" },
-    { songName: "Ghost-Ava Max", songPath: "songs/Ghost-Ava Max.mp3", songCover: "covers/4.jpg" },
-    { songName: "Heaven", songPath: "songs/Heaven.mp3", songCover: "covers/5.jpg" },
-    { songName: "One Direction Steal My Girl", songPath: "songs/One Direction Steal My Girl.mp3", songCover: "covers/6.jpg" },
-    { songName: "Our Song", songPath: "songs/Our Song.mp3", songCover: "covers/7.jpg" },
-    { songName: "rocket Science", songPath: "songs/rocket Science.mp3", songCover: "covers/8.jpg" },
-    { songName: "Snap", songPath: "songs/Snap.mp3", songCover: "covers/9.jpg" },
-    { songName: "You broke me first", songPath: "songs/You broke me first.mp3", songCover: "covers/10.jpg" }
+    { songName: "8 Letters- Why don't we", songPath: "songs/1.mp3", songCover: "covers/1.jpg", songDuration: "03:10" },
+    { songName: "Anti Hero", songPath: "songs/2.mp3", songCover: "covers/2.jpg", songDuration: "05:09" },
+    { songName: "closer", songPath: "songs/3.mp3", songCover: "covers/3.jpg", songDuration: "04:20" },
+    { songName: "Ghost-Ava Max", songPath: "songs/4.mp3", songCover: "covers/4.jpg", songDuration: "03:00" },
+    { songName: "Heaven", songPath: "songs/5.mp3", songCover: "covers/5.jpg", songDuration: "03:11" },
+    { songName: "One Direction- Steal My Girl", songPath: "songs/6.mp3", songCover: "covers/6.jpg", songDuration: "05:17" },
+    { songName: "Our Song-tylor Shift", songPath: "songs/7.mp3", songCover: "covers/7.jpg", songDuration: "03:04" },
+    { songName: "rocket Science", songPath: "songs/8.mp3", songCover: "covers/8.jpg", songDuration: "03:22" },
+    { songName: "Snap", songPath: "songs/9.mp3", songCover: "covers/9.jpg", songDuration: "03:14" },
+    { songName: "You broke me first", songPath: "songs/10.mp3", songCover: "covers/10.jpg", songDuration: "02:47" }
 ]
+let index = 1;
+let music = new Audio(songs[index - 1].songPath);
+masterSong.innerText = songs[index - 1].songName;
 
 songItems.forEach((element, i) => {
-    // console.log(element, i);
     element.getElementsByTagName('img')[0].src = songs[i].songCover;
     element.getElementsByClassName('songName')[0].innerText = songs[i].songName;
+    element.getElementsByClassName('timeStamp')[0].innerText = songs[i].songDuration;
 })
-
+const playMusic = (index) => {
+    music.play();
+    masterPlay.classList.remove('fa-play-circle');
+    masterPlay.classList.add('fa-pause-circle');
+    masterSong.innerText = songs[index - 1].songName;
+    gif.style.opacity = 1;
+}
+const pauseMusic = () => {
+    music.pause();
+    masterPlay.classList.remove('fa-pause-circle');
+    masterPlay.classList.add('fa-play-circle');
+    gif.style.opacity = 0;
+}
 // Adding Event Listeners------------------------------------------------------------
 masterPlay.addEventListener('click', () => {
     if (music.paused || music.currentTime <= 0) {
-        music.play();
-        masterPlay.classList.remove('fa-play-circle');
-        masterPlay.classList.add('fa-pause-circle');
-        gif.style.opacity = 1;
+
+        playMusic(index);
     }
     else {
-        music.pause();
-        masterPlay.classList.remove('fa-pause-circle');
-        masterPlay.classList.add('fa-play-circle');
-        gif.style.opacity = 0;
+
+        pauseMusic();
     }
 })
 
 
 music.addEventListener('timeupdate', () => {
-    // console.log('timeupdate');
+
     //updating progress Bar of music
-    // console.log(music.currentTime);
+
     let progress = parseInt((music.currentTime / music.duration) * 100);
     progressBar.value = progress;
+    // let cTime = Math.floor(music.currentTime);
+    // progressTime.innerText = cTime;
 
 })
 progressBar.addEventListener('change', () => {
     music.currentTime = (progressBar.value * music.duration) / 100;
+
 })
 const makeAllPlays = () => {
-    element.classList.remove('fa-pause-circle');
-    element.classList.add('fa-play-circle');
-}
-arrayFrom(document.getElementsByClassName('songItemPlay')).forEach((element) => {
-    element.addEventListener('click', (e) => {
-        makeAllPlays();
-        element.classList.remove('fa-play-circle');
-        element.classList.add('fa-pause-circle');
-        Index = parse(e.target.id);
-        music.src = 'songs/${index}.mp3';
-        music.currentTime = 0;
-        music.play();
-        masterPlay.classList.remove("fa-play-circle");
-        masterPlay.classList.add('fa-pause-circle');
-
+    Array.from(document.getElementsByClassName('songItemPlay')).forEach((element) => {
+        element.classList.remove('fa-pause-circle');
+        element.classList.add('fa-play-circle');
     })
+}
+Array.from(document.getElementsByClassName('songItemPlay')).forEach((element) => {
+    element.addEventListener('click', (e) => {
+
+        makeAllPlays();
+        e.target.classList.remove('fa-play-circle');
+        e.target.classList.add('fa-pause-circle');
+        index = parseInt(e.target.id);
+        music.src = songs[index - 1].songPath;
+        music.currentTime = 0;
+        playMusic(index);
+    })
+})
+document.getElementById('next').addEventListener('click', () => {
+    if (index >= 10) {
+        index = 1;
+    }
+    else {
+        index += 1;
+    }
+    makeAllPlays();
+    music.src = songs[index - 1].songPath;
+    music.currentTime = 0;
+    playMusic(index);
+})
+
+document.getElementById('previous').addEventListener('click', () => {
+    if (index <= 1) {
+        index = 1;
+    }
+    else {
+        index -= 1;
+    }
+    makeAllPlays();
+    music.src = songs[index - 1].songPath;
+    music.currentTime = 0;
+    playMusic(index);
 })
